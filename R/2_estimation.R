6dfd41979ef81acbb645c16be760d11da3f93ff4
Rubin Jean <AYDYV0@ad.insee.intra>
2024-03-17 15:37:17 +0100

    Balanced sampling variance estimation

--- R/2_estimation.R
+++ R/2_estimation.R
@@ -3,3 +3,20 @@ estimate_total <- function(sample) {
     summarize(y_hat = sum(y / pi_i * s_i)) |>
     pull(y_hat)
 }
+
+estimate_variance_balanced <- function(sample_pop, x_names, tol = 1e-6) {
+  x <- as.matrix(sample_pop[x_names])
+  y <- as.matrix(sample_pop$y)
+  pi_i <- sample_pop$pi_i
+  x_diag_p <- t(x / pi_i)
+  beta_hat <- pseudo_inv(x_diag_p %*% x, tol) %*% x_diag_p %*% y
+  e <- (y - x %*% beta_hat) / pi_i
+
+  t(e) %*% e
+}
+
+
+
+
+
+
