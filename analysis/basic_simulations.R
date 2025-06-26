a2f3c438e095c89091e40ab80e0f03d146734400
jeanrubin <jean.rubin@insee.fr>
2025-06-26 16:58:32 +0000

    Update basic simulation script

--- analysis/basic_simulations.R
+++ analysis/basic_simulations.R
@@ -3,11 +3,11 @@ library(dplyr)
 tar_source()
 tar_load(population)
 n_iter_true <- 1000L
-x_names <- c("x1", "pi_i_aux")
+x_names <- c("pi_i_aux", "const", "x1", "x2", "x3")
 n_samples <- c(50, 100, 200, 500, 750)
 
 sample_fn_list <- list(
-  base = sampler_gen(x_names),
+  base = sampler_gen_base(x_names),
   # wr_copy = sampler_gen_wr_copy(x_names), # very costly
   srswor = sampler_gen_srswor()
 )
@@ -17,8 +17,8 @@ v_approx_fn_list <- list(
   v_deville = compute_v_approx_deville_tille
 )
 
-sample_fuzzy_fn_list <- list(
-  wr_fuzzy = sampler_gen_fuzzy_wr(x_names)
+sample_flight_fn_list <- list(
+  wr_flight_exh = sampler_gen_flight_wr_exh(x_names)
 )
 
 # Check Baseline simulation for different sample size --------------------------
@@ -56,7 +56,7 @@ design_df <- tibble(
 
 # Flight phase analysis --------------------------------------------------------
 
-fuzzy_design_df <- tibble(
+flight_design_df <- tibble(
   n_sample = c(50, 100, 200, 300, 400, 500, 750)
 ) |>
   mutate(
@@ -67,7 +67,7 @@ fuzzy_design_df <- tibble(
         population <- set_inclusion_proba(population, pi_gen)
 
         c(
-          compute_v_trues(population, sample_fuzzy_fn_list, n_iter_true),
+          compute_v_trues(population, sample_flight_fn_list, n_iter_true),
           compute_v_approxs(population, v_approx_fn_list, x_names)
         )
       },
@@ -86,3 +86,15 @@ fuzzy_design_df <- tibble(
 # 5      400    2840.   2567.     1544.
 # 6      500    1910.   2054.     1029.
 # 7      750    1304.   1369.      343.
+
+
+# n_iter_true : 1000
+#   n_sample wr_flight_exh v_multi v_deville
+#      <dbl>         <dbl>   <dbl>     <dbl>
+# 1       50        21130.  20740.    19802.
+# 2      100        10774.  10370.     9380.
+# 3      200         5284.   5185.     4169.
+# 4      300         3355.   3457.     2432.
+# 5      400         2451.   2592.     1563.
+# 6      500         1883.   2074.     1042.
+# 7      750         1065.   1383.      347.
