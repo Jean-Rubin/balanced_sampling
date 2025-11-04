16e085e09a82baacddc4fb3606e533617f016f15
jeanrubin <jean.rubin@insee.fr>
2025-11-04 16:30:53 +0000

    Fix and add tests for jumping steps

--- tests/testthat/test-1_sample_population.R
+++ tests/testthat/test-1_sample_population.R
@@ -8,7 +8,6 @@ population_test_1 <- tibble::tibble(
   pi_i_aux = c(0.5, 0.5, 0.5, 0.5)
 )
 X_1 <- cbind(population_test_1$x, population_test_1$pi_i_aux)
-sample_test_size <- sum(population_test_1$pi_i)
 
 # Unequal probability, not an integer sample size
 population_test_2 <- tibble::tibble(
@@ -18,9 +17,54 @@ population_test_2 <- tibble::tibble(
   pi_i_aux = c(0.2, 0.8, 0.1, 0.4)
 )
 X_2 <- cbind(population_test_2$x, population_test_2$pi_i_aux)
-sample_test_size <- sum(population_test_2$pi_i)
 
 # Tests -----------------------------------------------------------------------
+
+describe("jump_wr_exh()", {
+  set.seed(1234)
+  for (k in seq_len(10)) {
+    jump_result_1 <- jump_wr_exh(X_1 / population_test_1$pi_i, population_test_1$pi_i, eps)
+
+    it("fixes the jumping coordinate to an integer" |> add_context(k),
+      expect_gt(
+        sum(abs(jump_result_1 - round(jump_result_1)) < eps),
+        sum(abs(population_test_1$pi_i - round(population_test_1$pi_i)) < eps)
+      )
+    )
+    it("gives positive jumping probabilities" |> add_context(k),
+      expect_true(all(jump_result_1 > -eps))
+    )
+    it("verifies the balancing constraints" |> add_context(k),
+      expect_equal(colSums(X_1 * jump_result_1 / population_test_1$pi_i), colSums(X_1))
+    )
+
+
+    jump_result_2 <- jump_wr_exh(X_2 / population_test_2$pi_i, population_test_2$pi_i, eps)
+
+    it("fixes the jumping coordinate to an integer" |> add_context(k),
+      expect_gt(
+        sum(abs(jump_result_2 - round(jump_result_2)) < eps),
+        sum(abs(population_test_2$pi_i - round(population_test_2$pi_i)) < eps)
+      )
+    )
+    it("gives positive jumping probabilities" |> add_context(k),
+      expect_true(all(jump_result_2 > -eps))
+    )
+    it("verifies the balancing constraints" |> add_context(k),
+      expect_equal(colSums(X_2 * jump_result_2 / population_test_2$pi_i), colSums(X_2))
+    )
+
+  }
+
+  it("works on edge case" |> add_context(k), {
+    X <- matrix(c(1, 1, 1), ncol = 1)
+    pik <- c(1e-13, 0.9, 0)
+    pik_2 <- c(-1e-13, 0.9, 0)
+
+    expect_no_error(jump_wr_ent(X, pik, eps = 1e-11))
+    expect_no_error(jump_wr_ent(X, pik_2, eps = 1e-11))
+  })
+})
 
 describe("flight_wr_exh()", {
   nb_aux_var_1 <- dim(X_1)[2]
@@ -72,8 +116,8 @@ describe("sample_cube_wr_exh()", {
 
 describe("jump_wr_ent()", {
   set.seed(1234)
-  for (k in seq_len(3)) {
-    jump_result_1 <- jump_wr_ent(X_1, population_test_1$pi_i, eps)
+  for (k in seq_len(10)) {
+    jump_result_1 <- jump_wr_ent(X_1 / population_test_1$pi_i, population_test_1$pi_i, eps)
     n_select_1 <- jump_result_1$pi_jump[jump_result_1$k_select]
 
     it("fixes the jumping coordinate to an integer" |> add_context(k),
@@ -86,7 +130,7 @@ describe("jump_wr_ent()", {
       expect_equal(colSums(X_1 * jump_result_1$pi_jump / population_test_1$pi_i), colSums(X_1))
     )
 
-    jump_result_2 <- jump_wr_ent(X_2, population_test_2$pi_i, eps)
+    jump_result_2 <- jump_wr_ent(X_2 / population_test_2$pi_i, population_test_2$pi_i, eps)
     n_select_2 <- jump_result_2$pi_jump[jump_result_2$k_select]
 
     it("fixes the jumping coordinate to an integer (2)" |> add_context(k),
